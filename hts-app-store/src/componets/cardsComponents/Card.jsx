8710dcc7f25d9a45488611504b780829660931e9
AleHts29 <ahuertas@fi.uba.ar>
2021-07-07 09:53:41 -0300

    Avance_1 proyecto

---
+++ hts-app-store/src/componets/cardsComponents/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import img1 from './assets/iphone12.jpeg';
+import './styls.css'
+import {ButtonComponent} from '../ButtonComponent'
+import {ItemComponent} from '../buttonCountComponent/ItemCount'
+const CardItemsComponent = ({productData, children}) => {
+    return (
+
+    <>
+    
+        <div className="col-12 mb-3 col-md-4 ">
+            <div className = "card bg-dark  mt-5">    
+                <img src={productData.img} alt="" className="tamanoImg"/>
+                <div className = "card-body text-center">
+                    <h5 className = "card-title text-white"> {productData.title}</h5>
+                    <strong className="text-warning">Price: $ {productData.price}</strong>
+                </div>
+                <div className="m-3 text-center">
+                    {children}
+                </div>
+            </div>  
+        </div>
+    
+    </>
+    )
+}
+
+export default CardItemsComponent
+
+
